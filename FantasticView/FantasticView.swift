9167f6276a59cb5c2feceaede8bdb8cc57d2ec95
loctv <loc.tran@ascendcorp.com>
2018-01-04 16:44:57 +0700

    init

---
+++ FantasticView/FantasticView.swift
@@ -0,0 +1,33 @@
+//
+//  FantasticView.swift
+//  FantasticView
+//
+//  Created by loctv on 1/4/18.
+//  Copyright © 2018 loctv. All rights reserved.
+//
+
+import UIKit
+
+class FantasticView: UIView {
+    
+    let colors : [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
+    var colorCounter = 0
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let scheduledColorChanged = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { (timer) in  //1
+            UIView.animate(withDuration: 2.0) {  //2
+                self.layer.backgroundColor = self.colors[self.colorCounter % 6].cgColor  //3
+                self.colorCounter+=1  //4
+            }
+        }
+        
+        scheduledColorChanged.fire()  //5
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        // You don't need to implement this
+    }
+}
--- FantasticView/ViewController.swift
+++ FantasticView/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let fantasticView = FantasticView(frame: self.view.bounds)
+        self.view.addSubview(fantasticView)
     }
 
     override func didReceiveMemoryWarning() {
